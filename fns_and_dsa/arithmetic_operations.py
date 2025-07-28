b7607d42742cd162b1bd618216870da8f67167cc
iambiruk <brookasefa0@gmail.com>
2025-07-28 09:37:08 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,5 +8,6 @@ def perform_operation(num1, num2, operation):
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Cannot divide by zero"
+        return num1 / num2
     else:
         return "Error: Invalid operation"
