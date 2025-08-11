97876bff763bc9e6f40eadcde929206f76fab2fb
iambiruk <brookasefa0@gmail.com>
2025-08-11 10:59:51 +0300

    Update main.py

--- oop/main.py
+++ oop/main.py
@@ -1,8 +1,9 @@
 from book_class import Book
 def main():
-    my_book = Book("1984", "George Orwell", "1949")
+    my_book = Book("1984", by "George Orwell", published in "1949")
     print(my_book)
     print(repr(my_book))
     del my_book
 if __name__ == "__main__":
+
     main
