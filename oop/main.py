1c21db0f135df858bc90360e8ef746ed713d2cdc
iambiruk <brookasefa0@gmail.com>
2025-08-11 23:25:53 +0300

    Add files via upload

--- oop/main.py
+++ oop/main.py
@@ -1,8 +1,14 @@
-from class_static_methods_demo import Calculator
+from library_system import Book, EBook, PrintBook, Library
 def main():
-    sum_result = Calculator.add(10, 5)
-    print(f"The sum is: {sum_result}")
-    product_result = Calculator.multiply(10, 5)
-    print(f"The product is: {product_result}")
+    my_library = Library()
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Cather in the Rye", "J.D Salinger", 234)
+
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
+
+    my_library.list_books()
 if __name__ == "__main__":
     main()
